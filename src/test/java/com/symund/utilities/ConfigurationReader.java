c4bc03b9b3f30f7c15db4ee2050ac2cf2576a253
OmerfFE <omerfaruk.erturk@hotmail.com>
2022-06-29 16:35:41 +0300

    Drivers are created

--- src/test/java/com/symund/utilities/ConfigurationReader.java
+++ src/test/java/com/symund/utilities/ConfigurationReader.java
@@ -1,18 +1,29 @@
 package com.symund.utilities;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationReader {
 
-    static Properties prop = new Properties()
+    static Properties prop = new Properties();
 
     static {
 
-        FileInputStream file = new FileInputStream("configuration.properties");
-        prop.load(file);
-        file.close();
+        try {
+
+            FileInputStream file = new FileInputStream("configuration.properties");
+            prop.load(file);
+            file.close();
+
+        } catch (IOException e) {
+            System.out.println("ConfigurationReader problem check it!");
+
+        }
     }
 
+    public static String getProperty(String keyword) {
+        return prop.getProperty(keyword);
+    }
 
 }
